1339f6be755828828ede1c5677d42bfa7001408e
STUST <STUST@STUST-34>
2023-09-19 15:22:02 +0800

    判斷質數與倍數

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -24,5 +24,72 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            int number;
+            List<int> primes = new List<int>();
+            bool success = int.TryParse(textbox1.Text, out number);
+            if(!success) 
+            {
+                MessageBox.Show("請輸入整數","輸入錯誤");
+            }
+            else if(number < 2)
+            {
+                MessageBox.Show($"輸入數值為{number}，小於2，請重新輸入", "輸入錯誤");
+            }
+            else
+            {
+                for(int i = 2; i <= number; i ++)
+                {
+                    if(IsPrime(i)) primes.Add(i);
+                }
+            }
+
+            //顯示所有質數與倍數
+            ListResult(primes, number);
+        }
+
+        private void ListResult(List<int> myprimes, int mynumber)
+        {
+            string primeList = $"小於等於{mynumber}的質數為：";
+            string primeMultiple = "";
+
+            foreach(int p in myprimes) 
+            {
+                primeList += $"，{p}  ";
+                primeMultiple += $"{p}的倍數為:  ";
+                int i = 1;
+                while(p*i <= mynumber) 
+                {
+                    primeMultiple += $"，{p * i}";
+                    i++;
+                }
+                primeMultiple += "\n";
+            
+            }
+            textblock1.Text = primeList;
+            textblock2.Text = primeMultiple;  
+        }
+
+        private bool IsPrime(int p)
+        {
+            //  判斷p是否為質數
+            for(int i = 2; i < p; i++)
+            {
+                if (p % i == 0) return false;
+            }
+            return true;
+        }
     }
 }
